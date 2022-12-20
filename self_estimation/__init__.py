d609c4eb8edbc5c9f1c4a0f96e835204e526120c
Markus Eyting <eyting.ma@gmail.com>
2022-12-20 13:39:28 +0100

    instructions, layout

--- self_estimation/__init__.py
+++ self_estimation/__init__.py
@@ -10,6 +10,7 @@ class C(BaseConstants):
     NAME_IN_URL = 'self_estimation'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
+    num_rounds_task = 50
 
 
 class Subsession(BaseSubsession):
@@ -22,17 +23,15 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     est_math = models.IntegerField(
-        label='How many of the 50 questions in the <b> math task </b> do you think you have answered correctly?',
         min=0,
         max=50,
     )
     est_avg_math = models.BooleanField(
-        label='Do you think you achieved a higher score in the <b> math task </b> than the average of all participants in our experiment?',
+        label='Do you think you achieved a higher score in the <b> adding task </b> than the average of all participants in our experiment?',
         choices=[[0, 'yes'], [1, 'no']],
         widget=widgets.RadioSelectHorizontal()
     )
     est_sequence = models.IntegerField(
-        label='How many of the 50 questions in the <b> sequence task </b> do you think you have answered correctly?',
         min=0,
         max=50,
     )
